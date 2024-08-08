21e4e15e67876829bfe12205f1a9d99e740207e7
Guilherme Anjos <guilherme.macedo1598@gmail.com>
2024-08-08 15:36:42 -0300

    first commit

---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,32 @@
+const screen = {
+    usertProfile: document.querySelector('.profile-data'),
+    renderUser(user) {
+        this.usertProfile.innerHTML =
+            `<div class="info">
+                <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
+                <div class="data">
+                    <h1>${user.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                    <p>${user.bio ?? 'Não possui bio cadastrada 😢'}</p>
+                </div>
+            </div>
+            `
+
+        let repositoriesItens = ''
+        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+
+        if(user.repositories.length > 0){
+            this.usertProfile.innerHTML += `
+            <div class="repositories section">
+                <h2> Repositórios</h2>
+                <ul>${repositoriesItens}</ul>
+            </div>
+            `
+        }
+    },
+    renderNotFound(){
+        this.usertProfile.innerHTML="<h3>usuário não encontrado</h3>"
+    }
+}
+
+
+export { screen }
